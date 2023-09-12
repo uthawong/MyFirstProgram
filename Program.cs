8049b106639b734a44475a8cc28e8e42e46cd4a7
Caroline <Caroline@LAPTOP-T9O2GLMC>
2023-09-12 11:12:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// Caroline Uthawong-Burr .NET23
+
+namespace MyFirstProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var MyNumber = 16; //This is a variable declaration
+            if (MyNumber > 10)
+            {
+                Console.WriteLine("Talet är stort!");
+            }
+            else
+            {
+                Console.WriteLine("Oj! Lågt tal!");
+            }
+            Console.WriteLine("Vad heter du?");
+            var Name = Console.ReadLine(); // Store user input
+            Console.WriteLine("Hej " + Name);
+
+            var MyCounting = 0;
+            while (MyNumber >= MyCounting) //The while loop finishes one round too early without the equal sign.
+            {
+                Console.WriteLine(MyCounting);
+                MyCounting++;
+            }
+        }
+    }
+}
